0d626282bec00a4a65b3e14f816a7b059a1fd841
Luis Rey Lara <luisreylara@hotmail.com>
2024-09-10 09:57:59 -0600

    Add files via upload

---
+++ code/holaMundo/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MiApp());
+}
+
+class MiApp extends StatelessWidget {
+  const MiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Center(
+        child: Text('Hola 9A IRT UPJR'),
+      ),
+    );
+  }
+}
