16df317bf11884dfc55951e253214f7d720d2a61
Luis Rey Lara <luisreylara@hotmail.com>
2024-09-10 23:58:18 -0600

    Update dynamic.dart

--- code/DART/dynamic.dart
+++ code/DART/dynamic.dart
@@ -1,1 +1,19 @@
 
+void main() {
+// dynamic == null
+
+  dynamic errorMensaje = 'Hola';
+  errorMensaje = 123;
+  errorMensaje = 'Luis';
+  errorMensaje = [1, 2, 3];
+  errorMensaje = {1, 2, 3};
+  errorMensaje = () => true;
+  errorMensaje = null;
+
+  errorMensaje += 1;
+
+  print("""
+
+ 
+""");
+}
