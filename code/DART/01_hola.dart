e09f13ec900de9dae2c95011a7de58b5e745c03c
Luis Rey Lara <luisreylara@hotmail.com>
2024-09-10 23:33:59 -0600

    Create 01_hola.dart

---
+++ code/DART/01_hola.dart
@@ -0,0 +1,12 @@
+void main() {
+  var miNombre = 'Luis';
+  String ap = 'Lara';
+  final am = 'Gonzalez';
+
+  //interpolación de String
+  print('Hola: $miNombre $ap $am');
+
+  print('Apellido en mayusculas $ap.toUpeercase()');
+  print('Apellido en mayusculas ${ap.toUpperCase()}');
+  print('suma: ${1 + 1}');
+}
