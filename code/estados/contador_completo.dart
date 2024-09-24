51898b670052b7f2625d375488daff1a3233a8c5
Luis Rey Lara <luisreylara@hotmail.com>
2024-09-24 08:08:43 -0600

    Create contador_completo.dart

---
+++ code/estados/contador_completo.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class MiScreenFunciones extends StatefulWidget {
+  const MiScreenFunciones({super.key});
+
+  @override
+  State<MiScreenFunciones> createState() => _MiScreenFuncionesState();
+}
+
+class _MiScreenFuncionesState extends State<MiScreenFunciones> {
+  int contaClicks = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                contaClicks++;
+              });
+            },
+            child: Icon(Icons.plus_one_outlined),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton(
+            shape: StadiumBorder(),
+            onPressed: () {
+              setState(() {
+                contaClicks--;
+              });
+            },
+            child: Icon(Icons.exposure_minus_1_outlined),
+          ),
+        ],
+      ),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  contaClicks = 0;
+                });
+              },
+              icon: Icon(Icons.restart_alt_outlined)),
+        ],
+        title: const Text('Material App Bar'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$contaClicks',
+              style: TextStyle(fontSize: 100),
+            ),
+            Text(
+              'Click${contaClicks == 1 ? '' : 's'} ',
+              style: TextStyle(fontSize: 20),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
