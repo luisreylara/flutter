1ac38be9b37c1bd9a83d1197e51c302df86b3f01
Luis Rey Lara <luisreylara@hotmail.com>
2024-09-24 07:51:52 -0600

    Create estados_appbar_iconIzq.dart

---
+++ code/estados/estados_appbar_iconIzq.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MiScreenFunciones extends StatefulWidget {
+  const MiScreenFunciones({super.key});
+
+  @override
+  State<MiScreenFunciones> createState() => _MiScreenFuncionesState();
+}
+
+class _MiScreenFuncionesState extends State<MiScreenFunciones> {
+  int contaClicks = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            contaClicks++;
+          });
+        },
+        child: Icon(Icons.plus_one_outlined),
+      ),
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {}, icon: Icon(Icons.restart_alt_outlined)),
+        title: const Text('Material App Bar'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$contaClicks',
+              style: TextStyle(fontSize: 100),
+            ),
+            Text(
+              'Click${contaClicks == 1 ? '' : 's'} ',
+              style: TextStyle(fontSize: 20),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
