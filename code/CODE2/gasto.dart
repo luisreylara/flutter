33a8523a854c1ca39932991141e9dd86f4f2e9db
Luis Rey Lara <luisreylara@hotmail.com>
2024-10-02 07:51:13 -0600

    Add files via upload

---
+++ code/CODE2/gasto.dart
@@ -0,0 +1,37 @@
+//agregar $flutter pub add uuid   https://pub.dev/packages/uuid
+// instalar: flutter pub add intl  https://pub.dev/packages/intl
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+final formateador = DateFormat.yMMMd();
+
+enum Categoria { comida, viaje, ocio, trabajo }
+
+const categoriaIconos = {
+  Categoria.comida: Icons.lunch_dining,
+  Categoria.viaje: Icons.flight_takeoff,
+  Categoria.ocio: Icons.movie,
+  Categoria.trabajo: Icons.work
+};
+
+class Gasto {
+  final String id;
+  final String titulo;
+  final double monto;
+  final DateTime fecha;
+  final Categoria categoria;
+
+  Gasto(
+      {required this.titulo,
+      required this.monto,
+      required this.fecha,
+      required this.categoria})
+      : id = uuid.v4();
+
+  String get FormatearFecha {
+    return formateador.format(fecha);
+  }
+}
